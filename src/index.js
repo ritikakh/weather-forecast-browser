7d331acbe60a890ccb76f5829933bcb76c4c5b04
ritikakh <ritikakh@usc.edu>
2018-05-06 12:00:29 -0700

    integrate redux-devtools-extension

--- src/index.js
+++ src/index.js
@@ -2,15 +2,23 @@ import React from 'react';
 import ReactDOM from 'react-dom';
 import { Provider } from 'react-redux';
 import { createStore, applyMiddleware } from 'redux';
+import { composeWithDevTools } from 'redux-devtools-extension';
+
 import ReduxPromise from 'redux-promise';
 
 import App from './components/app';
 import reducers from './reducers';
 
-const createStoreWithMiddleware = applyMiddleware(ReduxPromise)(createStore);
+const store = createStore(reducers, composeWithDevTools(
+    applyMiddleware(ReduxPromise)
+));
 
 ReactDOM.render(
-  <Provider store={createStoreWithMiddleware(reducers)}>
+  <Provider store={store}>
     <App />
   </Provider>
   , document.querySelector('.container'));
+
+
+
+
