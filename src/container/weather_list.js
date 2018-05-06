16677942277c889f4d79a4c5485817fa05661a3a
ritikakh <ritikakh@usc.edu>
2018-05-06 14:09:34 -0700

    Implemented the graphs for forecast

--- src/container/weather_list.js
+++ src/container/weather_list.js
@@ -1,11 +1,28 @@
 import React, { Component } from 'react';
 import { connect } from 'react-redux';
+import Chart from '../components/chart';
+
 
 class WeatherList extends Component {
     renderWeather(cityData) {
+
+        const temps = cityData.list.map(weather => weather.main.temp);
+        const humidity = cityData.list.map(weather => weather.main.humidity);
+        const pressure=  cityData.list.map(weather => weather.main.pressure);
+
+
         return (
             <tr key={cityData.city.id}>
                 <td>{cityData.city.name}</td>
+                <td>
+                    <Chart data={temps} color="orange" units="K"/>
+                </td>
+                <td>
+                    <Chart data={humidity} color="red" units="hPa"/>
+                </td>
+                <td>
+                    <Chart data={pressure} color="green" units="%"/>
+                </td>
             </tr>
         )
     }
@@ -16,9 +33,9 @@ class WeatherList extends Component {
                 <thead>
                     <tr>
                         <th>City</th>
-                        <th>Temperature</th>
-                        <th>Pressure</th>
-                        <th>Humidity</th>
+                        <th>Temperature(K)</th>
+                        <th>Pressure(hPa)</th>
+                        <th>Humidity(%)</th>
                     </tr>
                 </thead>
                 <tbody>
