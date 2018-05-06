309234286717b5ed94fbbf81e3b26570f6b913ea
ritikakh <ritikakh@usc.edu>
2018-05-06 11:50:18 -0700

    search bar implementation for city

---
+++ src/actions/index.js
@@ -0,0 +1,16 @@
+import axios from 'axios';
+
+const API_KEY = "5361b0061d8e6d26651408b8d9bc6d91";
+const ROOT_URL = `http://api.openweathermap.org/data/2.5/forecast?appid=${API_KEY}`;
+
+export const FETCH_WEATHER = 'FETCH_WEATHER';
+
+export function fetchWeather (city) {
+    const url = `${ROOT_URL}&q=${city},us`;
+    const request = axios.get(url);
+
+    return {
+        type: FETCH_WEATHER,
+        payload: request
+    };
+}
